1ac6352980b3dea40c4c85b10340c30f82480dae
Braian <braian.vidal742@gmail.com>
2023-12-08 20:59:54 -0300

    count_digits

--- hand_command.py
+++ hand_command.py
@@ -1,15 +1,32 @@
 import cv2
 import numpy as np
-import HandTrakingModule as htm
+import hand_traking_module as htm
 import math
 import pyautogui
+import time
 
 ############################################################
 wCam, hCam = 640, 480
 ############################################################
 
-#1
-camera = 1
+############################################################
+# font 
+font = cv2.FONT_HERSHEY_SIMPLEX 
+  
+# org 
+org = (50, 50) 
+  
+# fontScale 
+fontScale = 1
+   
+# Blue color in BGR 
+color = (255, 0, 0) 
+  
+# Line thickness of 2 px 
+thickness = 2
+############################################################
+
+camera = 0
 
 cap = cv2.VideoCapture(camera)
 
@@ -18,10 +35,9 @@ cap.set(4, hCam)
 
 detector = htm.handDetector(detectionCon=0.7, maxHands=1)
 
-hand_open = False
-hand_open2 = False
+test = True
 
-while True:
+while test == True:
 
     success, img = cap.read()
 
@@ -30,81 +46,74 @@ while True:
     lmList = detector.findPosition(img, draw=False)
 
     if len(lmList) != 0:
+        
+        # THUMB_MCP
+        THM  = x10, y10 = lmList[2][1], lmList[2][2]
+        
+        # INDEX_FINGER_PIP
+        INP = x20, y10 = lmList[6][1], lmList[6][2]
+        
+        # MIDDLE_FINGER_PIP
+        MIP = x30, y10 = lmList[10][1], lmList[10][2]
+        
+        # RING_FINGER_PIP
+        RIP = x40, y10 = lmList[14][1], lmList[14][2]
+        
+        # PINKY_PIP
+        PIP = x50, y10 = lmList[18][1], lmList[18][2]
+        
+        #---------------------- TOP FINGER BELOW ----------------------
+        
+        # THUMB_TIP
+        THT = x11, y11 = lmList[4][1], lmList[4][2]
+        
+        # INDEX_FINGER_TIP
+        INT = x21, y21 = lmList[8][1], lmList[8][2]
+        
+        # MIDDLE_FINGER_TIP
+        MIT = x31, y31 = lmList[12][1], lmList[12][2]
+        
+        # RING_FINGER_TIP
+        RIT = x41, y41 = lmList[16][1], lmList[16][2]
+        
+        # PINKY_TIP
+        PIT = x51, y51 = lmList[20][1], lmList[20][2]
+        
+        # Circle in tip of finger
+        cv2.circle(img, THT, 5, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, INT, 5, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, MIT, 5, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, RIT, 5, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, PIT, 5, (255, 0, 255), cv2.FILLED)
 
-        # -------------------------------------------- Minimizar -------------------------------------------------------
-        x1, y1 = lmList[8][1], lmList[8][2]
-        x2, y2 = lmList[0][1], lmList[0][2]
+        # Variável para contar dedos levantados
+        dedos_levantados = 0
 
-        cv2.circle(img, (x1, y1), 15, (255,0,255), cv2.FILLED)
-        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+        # Comparação correta para verificar se o dedo está acima do indicador
+        if THT > THM:
+            dedos_levantados += 1
 
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        if INT[1] < INP[1]:
+            dedos_levantados += 1
 
-        lengh = math.hypot(x2 - x1, y2 - y1)
+        if MIT[1] < MIP[1]:
+            dedos_levantados += 1
 
-        abertPerc = np.interp(lengh, [50, 300], [0, 100])
+        if RIT[1] < RIP[1]:
+            dedos_levantados += 1
 
-        # verifica se mão está aberta, se está miniminiza todas as telas
-        if round(abertPerc) > 90:
-            if not hand_open:
-                hand_open = True
-                pyautogui.hotkey('win', 'd')
-        else:
-            hand_open = False
-        # -------------------------------------------- Minimizar -------------------------------------------------------
+        if PIT[1] < PIP[1]:
+            dedos_levantados += 1
 
-        # -------------------------------------------- Alt + Tab -------------------------------------------------------
-        x3, y3 = lmList[20][1], lmList[20][2]
-        x4, y4 = lmList[0][1], lmList[0][2]
+        # Exibir o número total de dedos levantados
+        cv2.putText(img, f'Dedos Levantados: {dedos_levantados}', (50, 50), font, fontScale, color, thickness, cv2.LINE_AA)
 
-        cv2.circle(img, (x3, y3), 15, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (x4, y4), 15, (255, 0, 255), cv2.FILLED)
 
-        cv2.line(img, (x3, y3), (x4, y4), (255, 0, 255), 3)
-
-        lengh2 = math.hypot(x4 - x3, y4 - y3)
-
-        abertPerc2 = np.interp(lengh2, [50, 300], [0, 100])
-        # --------------------------------------------------------------------------------------------------------------
-
-        x5, y5 = lmList[16][1], lmList[16][2]
-        x6, y6 = lmList[0][1], lmList[0][2]
-
-        cv2.circle(img, (x5, y5), 15, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (x6, y6), 15, (255, 0, 255), cv2.FILLED)
-
-        cv2.line(img, (x5, y5), (x6, y6), (255, 0, 255), 3)
-
-        lengh3 = math.hypot(x6 - x5, y6 - y5)
-
-        trocaPerc = np.interp(lengh3, [50, 300], [0, 100])
-
-        # --------------------------------------------------------------------------------------------------------------
-        print("Primeiro: ", round(abertPerc))
-        print("Segundo: ", round(abertPerc2))
-        print("Terceiro: ", round(trocaPerc))
-
-        # verifica se a mão está aberta para alternar entre as janelas
-        if round(abertPerc2) > 50:
-            if not hand_open2:
-                hand_open2 = True
-                pyautogui.keyDown('alt')
-                pyautogui.press('tab')
-                if round(trocaPerc) >= 25:
-                    pyautogui.keyUp('alt')
-
-                if round(trocaPerc) < 10:
-                    pyautogui.keyUp('enter')
-
-        else:
-            hand_open2 = False
-        # -------------------------------------------- Alt + Tab -------------------------------------------------------
-
-    cv2.rectangle(img, (50, 150), (85, 360), (255,0,0), 3)
-
+   
     cv2.imshow("img", img)
 
+    # Break if use ESC or ENTER
     k = cv2.waitKey(1) & 0xFF
-
     if k == 27 or k == 13:
-        break
+        print("You stop the program")
+        test = False
--- hand_traking_module.py
+++ hand_traking_module.py
@@ -21,41 +21,12 @@ class handDetector():
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-        # print(results.multi_hand_landmarks)
 
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
         return img
-
-
-    '''def findPosition(self, img, handNo=0, draw=True):
-        xList = []
-        yList = []
-        bbox = []
-        self.lmList = []
-        if self.results.multi_hand_landmarks:
-            myHand = self.results.multi_hand_landmarks[handNo]
-        for id, lm in enumerate(myHand.landmark):
-            # print(id, lm)
-            h, w, c = img.shape
-            cx, cy = int(lm.x * w), int(lm.y * h)
-            xList.append(cx)
-            yList.append(cy)
-            # print(id, cx, cy)
-            self.lmList.append([id, cx, cy])
-            if draw:
-                cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
-        xmin, xmax = min(xList), max(xList)
-        ymin, ymax = min(yList), max(yList)
-        bbox = xmin, ymin, xmax, ymax
-
-        if draw:
-            cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
-                          (bbox[2] + 20, bbox[3] + 20), (0, 255, 0), 2)
-
-        return self.lmList, bbox'''
 
     def findPosition(self, img, handNo=0, draw=True):
         """Lists the position/type of landmarks
@@ -84,23 +55,6 @@ class handDetector():
                     cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
         return lmlist
-
-
-    def fingersUp(self):
-        fingers = []
-        # Thumb
-        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
-            fingers.append(1)
-        else:
-            fingers.append(0)
-        # 4 Fingers
-        for id in range(1, 5):
-            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
-        return fingers
-
 
     def findDistance(self, p1, p2, img, draw=True):
         x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
